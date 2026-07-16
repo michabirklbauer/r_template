79cf8713d15a5916374de1dbd1fd5290ca56ce5b
Micha Johannes Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-07-16 17:56:01 +0200

    update shiny template

--- app.R
+++ app.R
@@ -1,43 +1,61 @@
 library(tidyverse)
 library(shiny)
+library(bslib)
 
-ui <- pageWithSidebar(
-    headerPanel = headerPanel("Clustering App"),
+ui <- fluidPage(
+  theme = bs_theme(bootswatch = "brite"),
+  titlePanel("Clustering App"),
+  sidebarLayout(
     sidebarPanel = sidebarPanel(
-        selectInput("top_x", "Length or Width", choices = c("Length", "Width"), selected = "Length"),
-        uiOutput("xselect"),
-        selectInput("top_y", "Length or Width", choices = c("Length", "Width"), selected = "Width"),
-        uiOutput("yselect")
-        #selectInput("x", "X-Axis", choices = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")),
-        #selectInput("y", "Y-Axis", choices = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"))
+      selectInput(
+        "top_x",
+        "Length or Width",
+        choices = c("Length", "Width"),
+        selected = "Length"
+      ),
+      uiOutput("xselect"),
+      selectInput(
+        "top_y",
+        "Length or Width",
+        choices = c("Length", "Width"),
+        selected = "Width"
+      ),
+      uiOutput("yselect")
+      #selectInput("x", "X-Axis", choices = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")),
+      #selectInput("y", "Y-Axis", choices = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"))
     ),
     mainPanel = mainPanel(
-        plotOutput("plot1")
+      plotOutput("plot1")
     )
+  )
 )
 
-server <- function(input, output, session){
+server <- function(input, output, session) {
+  output$xselect <- renderUI({
+    selection <- colnames(iris)[grepl(
+      pattern = paste0(input$top_x, "$"),
+      colnames(iris)
+    )]
 
-    output$xselect <- renderUI({
+    selectInput("x", "X-Axis", choices = selection, selected = "Sepal.Length")
+  })
 
-        selection <- colnames(iris)[grepl(pattern = paste0(input$top_x, "$"), colnames(iris))]
+  output$yselect <- renderUI({
+    selection <- colnames(iris)[grepl(
+      pattern = paste0(input$top_y, "$"),
+      colnames(iris)
+    )]
 
-        selectInput("x", "X-Axis", choices = selection, selected = "Sepal.Length")
-    })
+    selectInput("y", "Y-Axis", choices = selection, selected = "Sepal.Width")
+  })
 
-    output$yselect <- renderUI({
-
-        selection <- colnames(iris)[grepl(pattern = paste0(input$top_y, "$"), colnames(iris))]
-
-        selectInput("y", "Y-Axis", choices = selection, selected = "Sepal.Width")
-    })
-
-    output$plot1 <- renderPlot({
-        iris %>% ggplot(
-            aes(x = !!sym(input$x), y = !!sym(input$y), color=Species)
-        ) +
-        geom_point()
-    })
+  output$plot1 <- renderPlot({
+    iris %>%
+      ggplot(
+        aes(x = !!sym(input$x), y = !!sym(input$y), color = Species)
+      ) +
+      geom_point()
+  })
 }
 
 shinyApp(ui, server)
