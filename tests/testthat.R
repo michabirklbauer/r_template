c9a2a0101b0e80890a73976e4d96054613d84b63
Micha Johannes Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-07-19 00:44:35 +0200

    update testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,6 @@
+#!/usr/bin/env Rscript
+
 # This file is part of the standard setup for testthat.
-# It is recommended that you do not modify it.
 #
 # Where should you do additional test configuration?
 # Learn more about the roles of various files in:
