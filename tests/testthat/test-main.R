3bfbc81e3da20dbf33c5de5dc2493762ca9c1e14
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-07-21 15:50:51 +0200

    real test case

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -1,7 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
-
-test_that("multiplication doesn't work", {
-  expect_equal(2 * 2, 4)
+test_that("Simple Character creation works", {
+  expect_equal(
+    {
+      jb <- Character$new("John Baldur")
+      jb$name
+    },
+    "John Baldur"
+  )
 })
