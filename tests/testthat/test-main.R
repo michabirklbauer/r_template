b3e4ea8b5fcfbe2b388be365444b8bca13a71db7
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-07-21 16:24:33 +0200

    update tests

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -1,9 +1,57 @@
 test_that("Simple Character creation works", {
-  expect_equal(
+  expect_match(
     {
-      jb <- Character$new("John Baldur")
+      jb <- Character$new(name = "John Baldur")
       jb$name
     },
-    "John Baldur"
+    "John Baldur",
+    fixed = TRUE
+  )
+})
+
+test_that("Simple Character attack works", {
+  expect_equal(
+    {
+      jb <- Character$new(name = "John Baldur")
+      jb$attack()
+    },
+    0.0
+  )
+})
+
+test_that("Character factory works", {
+  expect_match(
+    {
+      characters <- character_factory("../../data/characters.csv")
+      characters[[1]]$name
+    },
+    "Astarion",
+    fixed = TRUE
+  )
+})
+
+test_that("Battle works", {
+  expect_match(
+    {
+      utils::capture.output({
+        characters <- character_factory("../../data/characters.csv")
+        winner <- battle(characters[[1]], characters[[2]], health = 10000)
+      })
+      winner$name
+    },
+    "Shadowheart",
+    fixed = TRUE
+  )
+})
+
+test_that("Running script works", {
+  expect_equal(
+    {
+      utils::capture.output({
+        exit_status <- main(c("-f", "../../data/characters.csv"))
+      })
+      exit_status
+    },
+    0
   )
 })
